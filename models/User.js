6165ab7871ad747fd888d878e28b101d0ef1a25f
nayala98 <nathaniel.ayala20@gmail.com>
2021-08-26 21:16:37 -0500

    update models

---
+++ models/User.js
@@ -0,0 +1,63 @@
+// set up imports
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
+
+//Set up object
+class User extends Model {
+  //check passwords
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password); // compare plaintextPassword with hased personal password
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // Validate if it is a valid email
+      validate: { isEmail: true },
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      //make sure password has to be at least 5 char
+      validate: { len: [5] },
+    },
+  },
+
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
